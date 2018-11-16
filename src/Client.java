ed565453baecf6f09cd11ea71e0ce1b5ab704974
alex-viman <alex.viman@gmail.com>
2018-11-16 12:35:18 -0500

    Final update

--- src/Client.java
+++ src/Client.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
 
@@ -30,11 +31,93 @@ public class Client {
 
 	public static void main(String[] args) throws Exception{
 
-		URL a_server = new URL("http://localhost:8000/ServiceA?wsdl");
-		URL b_server = new URL("http://localhost:9000/ServiceB?wsdl");
+		String arg0 = "";
+		String arg1 = "";
 
-		QName servNameA = new QName("http://localhost","ServiceA");
-		QName servNameB = new QName("http://localhost","ServiceB");
+		boolean dynamicMode = false;	//user-specified SOAP server address
+		boolean staticMode = false;		//hard-coded SOAP server addresses
+
+		//Had to initialize these early, compiler kept stating it wasn't initialized
+		URL a_server, b_server;
+		a_server = b_server = new URL("http://localhost/");
+		QName servNameA, servNameB;
+		servNameA = servNameB = new QName("", "");
+
+		switch (args.length) {
+			case 0:
+				staticMode = true;
+				break;
+			case 2:
+				dynamicMode = true;
+				break;
+		}
+
+		if (staticMode) {
+			a_server = new URL("http://localhost:8000/ServiceA?wsdl");
+			b_server = new URL("http://localhost:9000/ServiceB?wsdl");
+
+			servNameA = new QName("http://localhost", "ServiceA");
+			servNameB = new QName("http://localhost", "ServiceB");
+		}
+		else if (dynamicMode)
+		{
+			arg0 = args[0];
+			arg1 = args[1];
+
+			a_server = new URL(arg0);
+			b_server = new URL(arg1);
+
+			String x = "";
+
+			for (int j = 0; j < 2; j++) {
+				if (j == 0)
+					x = arg0;
+				if (j == 1)
+					x = arg1;
+
+				Scanner scan0 = new Scanner(x);
+				scan0.useDelimiter(":");
+
+				int i = 0;
+				String s1 = "";
+				String s2 = "";
+				while (scan0.hasNext()) {
+					switch (i) {
+						case 0:
+							s1 += scan0.next();
+							break;
+						case 1:
+							s1 += ":" + scan0.next();
+							break;
+						case 2:
+							s2 += scan0.next();
+							break;
+					}
+					i++;
+				}
+
+				scan0 = new Scanner(s2);
+				scan0.useDelimiter("/|\\?");
+				s2 = scan0.next();
+				s2 = scan0.next(); //not redundant, wanted the second value
+				scan0.close();
+
+				if (j == 0) {
+					servNameA = new QName(s1, s2);
+				}
+				if (j == 1) {
+					servNameB = new QName(s1, s2);
+				}
+			}
+		}
+		else {
+			System.out.println("There weren't enough arguments. Please try again or run this without any arguments for Static Mode!\n");
+			System.out.println("BUILD:   javac Client.java");
+			System.out.println("RUN:     java  Client <Service_Address1> <Service_Address2>	");
+			System.out.println("RUN:     java  Client");
+			System.out.println("EXAMPLE: java  http://localhost:8000/ServiceA?wsdl http://localhost:9000/ServiceB?wsdl");
+			System.exit(0);
+		}
 
 		Service a_service = Service.create(a_server, servNameA);
 		Service b_service = Service.create(b_server, servNameB);
